430a78b7a367ccfaa9ddd00ffaaddbe77885fc37
Ankit BK <ankitpanthi72@gmail.com>
2023-08-11 13:08:16 +0545

    Fixed Git issues

--- update.php
+++ update.php
@@ -1,5 +1,18 @@
-<html>
+<?php
+    session_start();
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <input type="text" name ="company-name" placeholder="Company Name">
+    <input type="text" name ="address" placeholder="Address">
+    <button type="submit">Update</button>
 
-
-
+    </form>
+</body>
 </html>
